ba239fa730fda63da8cd8242cca1950b3f002fd3
Nguyễn Xuân Thịnh <xuanthinhkl1@gmail.com>
2025-05-04 01:15:50 +0700

    update ui

---
+++ Assets/Loading/Scripts/LoadingManager.cs
@@ -0,0 +1,39 @@
+using SoundManager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingManager : MonoBehaviour
+{
+    public static LoadingManager instance;
+
+    [SerializeField] protected LoadingUI _loadingUI;
+
+    public LoadingUI LoadingUI { get { return _loadingUI; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        _loadingUI.gameObject.SetActive(false);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        //SoundsManager.Instance.StopMusic();
+
+        _loadingUI.gameObject.SetActive(true);
+        _loadingUI.LoadLevel(sceneName);
+    }
+}
---
+++ Assets/Loading/Scripts/LoadingUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingUI : MonoBehaviour
+{
+    [SerializeField] private Slider loadingSlider;
+    [SerializeField] private TextMeshProUGUI loadingText;
+
+    public void LoadLevel(string nameScene)
+    {
+        StartCoroutine(LoadAsyncchronously(nameScene));
+    }
+
+    IEnumerator LoadAsyncchronously(string nameScene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(nameScene);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            loadingSlider.value = progress;
+            loadingText.text = $"Loading...{progress*100}%";
+            yield return null;
+        }
+
+        this.gameObject.SetActive(false);
+    }
+}
