03b7e98d27b9a89aa09a53db0c7cece84be675ea
Nguyễn Xuân Thịnh <xuanthinhkl1@gmail.com>
2025-05-03 13:24:01 +0700

    update

---
+++ Assets/Main/Scripts/Sound/SoundsManager.cs
@@ -0,0 +1,106 @@
+using SoundManager;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace SoundManager
+{
+    public class SoundsManager : MonoSingleton<SoundsManager>
+    {
+        [SerializeField] private SoundSO SO;
+        public AudioSource musicSource;
+        public AudioSource sfxSource;
+
+        private float musicVolume;
+        private float sfxVolume;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            DontDestroyOnLoad(this);
+
+            SetMusicVolume(PlayerPrefs.GetFloat("Music Volume", 1));
+            SetSFXVolume(PlayerPrefs.GetFloat("SFX Volume", 1));
+        }
+
+        public float GetMusicVolume()
+        { 
+            return musicVolume; 
+        }
+        public float GetSFXVolume() 
+        {  
+            return sfxVolume; 
+        }
+
+        public void SetMusicVolume(float value)
+        {
+            musicVolume = value;
+            musicSource.volume = musicVolume;
+            PlayerPrefs.SetFloat("Music Volume", musicVolume);
+        }
+
+        public void SetSFXVolume(float value)
+        {
+            sfxVolume = value;
+            sfxSource.volume = sfxVolume;
+            PlayerPrefs.SetFloat("SFX Volume", sfxVolume);
+        }
+
+        public void PlaySFX(SoundType sound, AudioSource source = null)
+        {
+            SoundList soundList = SO.sounds[(int)sound];
+            AudioClip[] clips = soundList.sounds;
+            AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+
+            if (source)
+            {
+                source.outputAudioMixerGroup = soundList.mixer;
+                source.clip = randomClip;
+                source.volume = sfxVolume * soundList.volume;
+                source.Play();
+            }
+            else
+            {
+                sfxSource.outputAudioMixerGroup = soundList.mixer;
+                sfxSource.PlayOneShot(randomClip, sfxVolume * soundList.volume);
+            }
+        }
+
+        public void PlayMusic(SoundType sound, AudioSource source = null)
+        {
+            SoundList soundList = SO.sounds[(int)sound];
+            AudioClip[] clips = soundList.sounds;
+            AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+
+            if (source)
+            {
+                source.outputAudioMixerGroup = soundList.mixer;
+                source.clip = randomClip;
+                source.volume = musicVolume * soundList.volume;
+                source.Play();
+            }
+            else
+            {
+                musicSource.outputAudioMixerGroup = soundList.mixer;
+                musicSource.clip = randomClip;
+                musicSource.volume = musicVolume * soundList.volume;
+                musicSource.Play();
+            }
+        }
+
+        public void StopMusic()
+        {
+            musicSource.Stop();
+        }
+    }
+
+    [Serializable]
+    public struct SoundList
+    {
+        //[HideInInspector] 
+        public string name;
+        [Range(0, 1)] public float volume;
+        public AudioMixerGroup mixer;
+        public AudioClip[] sounds;
+    }
+}
