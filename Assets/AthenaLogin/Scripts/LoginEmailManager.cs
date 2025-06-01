f779f7206b0f03497754750e678b73a9f018eeac
Nguyễn Xuân Thịnh <xuanthinhkl1@gmail.com>
2025-06-01 16:22:39 +0700

    Update

--- Assets/AthenaLogin/Scripts/LoginEmailManager.cs
+++ Assets/AthenaLogin/Scripts/LoginEmailManager.cs
@@ -182,10 +182,10 @@ namespace OneID
             {
                 ShowRegisterNoticePopupUI("Password contain at least 1 upper case letter and 1 special letter", false);
             }
-            //else if (string.IsNullOrEmpty(_userEmail))
-            //{
-            //    ShowRegisterNoticePopupUI("Please fill in your information", false);
-            //}
+            else if (string.IsNullOrEmpty(_userEmail))
+            {
+                ShowRegisterNoticePopupUI("Please fill in your information", false);
+            }
             else
             {
                 if (Application.internetReachability == NetworkReachability.NotReachable)
@@ -198,7 +198,8 @@ namespace OneID
                     PlayFabClientAPI.AddUsernamePassword(new AddUsernamePasswordRequest
                     { Email = _userEmail, Password = _userPassword, Username = _username },
                     OnRegisterSuccess,
-                    OnRegisterFailure);
+                    OnRegisterFailure
+                    );
                 }
             }
 
